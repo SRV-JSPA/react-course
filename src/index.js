acfcf2531dfadc592123b86e75294899d5b5bbe7
JSPA <per22318@uvg.edu.gt>
2024-03-18 12:32:33 -0600

    react-icons and arrays management

--- src/index.js
+++ src/index.js
@@ -4,16 +4,32 @@ import { Greeting } from "./Greeting";
 import Product from "./Product";
 import { Button } from "./Button";
 import { TaskCard } from "./Task";
+import { Post } from "./Posts";
 
 const root = ReactDom.createRoot(document.getElementById("root"));
 
+const users = [
+  {
+    id: 1,
+    name: "Ryan ray",
+    image: "https://robohash.org/user1",
+  },
+  {
+    id: 2,
+    name: "Joe",
+    image: "https://robohash.org/user2",
+  },
+];
+
 root.render(
   <>
-    {/* <TaskCard ready={false}/> */}
-    <Button text="Hola" name='jose'/>
-
-    <input onChange={function (event){
-      console.log(event.target.value)
-    }} />
+    {users.map((user, i) => {
+      return (
+        <div>
+          <h1 key={i}>{user.name}</h1>
+          <img src={user.image} />
+        </div>
+      );
+    })}
   </>
 );
