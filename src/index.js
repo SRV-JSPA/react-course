d134529efc3208a7255003bb98b3d9d0df4e40a9
JSPA <per22318@uvg.edu.gt>
2024-03-19 16:09:38 -0600

    cambios finales

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDom from "react-dom/client";
 import { Greeting } from "./Greeting";
 import Product from "./Product";
@@ -8,28 +8,38 @@ import { Post } from "./Posts";
 
 const root = ReactDom.createRoot(document.getElementById("root"));
 
-const users = [
-  {
-    id: 1,
-    name: "Ryan ray",
-    image: "https://robohash.org/user1",
-  },
-  {
-    id: 2,
-    name: "Joe",
-    image: "https://robohash.org/user2",
-  },
-];
+function Mensaje() {
+  const [mensaje, setMensaje] = useState("");
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    console.log("render")
+  }, [counter])
+
+  return (
+    <div>
+      <input onChange={(event) => {
+        setMensaje(event.target.value)
+      }} ></input>
+      <button onClick={() => {
+        alert('el mensaje es:' + mensaje)
+      }} >Save</button>
+
+      <hr/>
+
+      <h1>Counter: {counter}</h1>
+      <button onClick={() => {
+        setCounter( counter + 1 )
+      }} >Incrementar</button>
+
+
+    </div>
+    
+  );
+}
 
 root.render(
   <>
-    {users.map((user, i) => {
-      return (
-        <div>
-          <h1 key={i}>{user.name}</h1>
-          <img src={user.image} />
-        </div>
-      );
-    })}
+    <Mensaje />
   </>
 );
