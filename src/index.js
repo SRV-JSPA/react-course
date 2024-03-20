3b16368cf55578e3bf8a6a40de3806d9a891cf17
JSPA <per22318@uvg.edu.gt>
2024-03-19 22:51:23 -0600

    api changes

--- src/index.js
+++ src/index.js
@@ -8,35 +8,41 @@ import { Post } from "./Posts";
 
 const root = ReactDom.createRoot(document.getElementById("root"));
 
+async function api (){
+  const datos = await fetch('http://localhost:3000/posts')
+  const json = await datos.json()
+
+  return json
+}
+
 function Mensaje() {
-  const [mensaje, setMensaje] = useState("");
-  const [counter, setCounter] = useState(0);
+  const [titulos, setTitulos] = useState([]);
 
   useEffect(() => {
-    console.log("render")
-  }, [counter])
+    const fetchData = async () => {
+      try {
+        const data = await api();
+        const titulos = data.map(item => item.title); 
+        setTitulos(titulos); 
+      } catch (error) {
+        console.error('Error al obtener los datos:', error);
+      }
+    };
+    
+    fetchData();
+
+    
+  }, []);
 
   return (
     <div>
-      <input onChange={(event) => {
-        setMensaje(event.target.value)
-      }} ></input>
-      <button onClick={() => {
-        alert('el mensaje es:' + mensaje)
-      }} >Save</button>
-
-      <hr/>
-
-      <h1>Counter: {counter}</h1>
-      <button onClick={() => {
-        setCounter( counter + 1 )
-      }} >Incrementar</button>
-
-
+      {titulos.map((titulo, index) => (
+        <h1 key={index}>{titulo}</h1>
+      ))}
     </div>
-    
   );
 }
+
 
 root.render(
   <>
